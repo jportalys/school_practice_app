1dc8a88cd4841f3a2260e42e0e4a9d4104907797
Justiceshrs Portal <jportal@you-source.com>
2019-07-25 16:14:26 +0800

    Add test for unenroll

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,5 @@
 class Enrollment < ApplicationRecord
+  
   belongs_to :student
   belongs_to :course
 
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Enrollment, type: :model do
-  
+  describe "public instance methods" do
+    context "it responds to its methods" do
+      it { is_expected.to respond_to(:unenroll) }
+    end
+
+    context "executes methods correctly" do
+      it "should call destroy with student is equal to enrollment student" do
+        student = create(:student)
+        enrollment = create(:enrollment, student: student, course: create(:course))       
+
+        expect(enrollment).to receive(:destroy)
+        enrollment.unenroll(student)
+      end
+
+      it "should not call destroy when student is not the student in the enrollment" do
+        student = create(:student)
+        enrollment = create(:enrollment, student: create(:student), course: create(:course))       
+
+        expect(enrollment).to_not receive(:destroy)
+        enrollment.unenroll(student)
+      end
+    end
+  end
 end
