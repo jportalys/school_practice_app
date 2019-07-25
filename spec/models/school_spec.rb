1d1953319bef839f4b3b29f267236c8929d9e1a6
Justiceshrs Portal <jportal@you-source.com>
2019-07-25 10:23:48 +0800

    remove unneccessary commented code

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -9,14 +9,5 @@ RSpec.describe School, type: :model do
 
   describe "Validations" do
     it { is_expected.to validate_presence_of(:name) }
-    # it { is_expected.to validate_presence_of(:email) }
-    # it { is_expected.to validate_presence_of(:password) }
-
-    # it { is_expected.to_not allow_value("johntest").for(:email) }
-    # it { is_expected.to_not allow_value("johntest.com").for(:email) }
-    # it { is_expected.to allow_value("john@test").for(:email) }
-    # it { is_expected.to allow_value("john@test.com").for(:email) }
-
-    # it { is_expected.to validate_confirmation_of(:password) }
   end
 end
